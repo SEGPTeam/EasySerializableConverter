1f517fdf18aa1fe579da6918ccca49aa8728e192
Blahlicus <blahlicus@gmail.com>
2018-11-10 03:23:15 +0800

    add: basic xml serialiser

--- EasySerializableConverterLib/MdCore.cs
+++ EasySerializableConverterLib/MdCore.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml.Serialization;
+using System.IO;
 namespace EasySerializableConverterLib
 {
     // helper function
     public class MdCore
     {
         // checks if an object is a list or something similar
-        public static bool IsEnumerable(object input)
+        public static bool IsEnumerableObject(object input)
         {
             bool output = false;
             if (input != null)
@@ -20,6 +21,40 @@ namespace EasySerializableConverterLib
                     output = true;
                 }
             }
+            return output;
+        }
+
+        public static bool IsEnumerableType(Type input)
+        {
+            bool output = false;
+            if (input != null)
+            {
+                if (typeof(System.Collections.IEnumerable).IsAssignableFrom(input))
+                {
+                    output = true;
+                }
+            }
+            return output;
+        }
+
+
+
+        //TODO We will want to replace these with custom serialisers
+        public static void Serialize<T>(T obj, string path)
+        {
+            var sw = new StreamWriter(path, false);
+            var ser = new XmlSerializer(typeof(T));
+            ser.Serialize(sw, obj);
+            sw.Close();
+        }
+
+        public static T Deserialize<T>(string path)
+        {
+            var sr = new StreamReader(path);
+            var ser = new XmlSerializer(typeof(T));
+            var output = (T)ser.Deserialize(sr);
+
+            sr.Close();
             return output;
         }
     }
