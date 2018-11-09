566b9aa670a5d060d1ece329727c35745e80d990
Blahlicus <blahlicus@gmail.com>
2018-11-09 19:37:15 +0800

    add: class instance data structure

---
+++ EasySerializableConverterLib/ClClassData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySerializableConverterLib
+{
+    public class ClClassData
+    {
+        public string ClassName { get; set; }
+        public string InstanceName { get; set; }
+        public List<ClFieldData> FieldDatas { get; set; }
+
+        public ClClassData(string instanceName, string className, List<ClFieldData> fieldDatas)
+        {
+            ClassName = className;
+            InstanceName = instanceName;
+
+            FieldDatas = new List<ClFieldData>();
+            foreach (var f in fieldDatas) // clones fieldDatas byVal to FieldData
+            {
+                FieldDatas.Add(f);
+            }
+        }
+
+        public ClClassData(string instanceName, ClClass classType) : this(instanceName, classType.ClassName, new List<ClFieldData>())
+        {
+            foreach (var f in classType.Fields)
+            {
+                FieldDatas.Add(new ClFieldData(f)); // populates field data list with new instances constructed from field data
+            }
+        }
+    }
+}
---
+++ EasySerializableConverterLib/ClFieldData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySerializableConverterLib
+{
+    public class ClFieldData
+    {
+        public ClField FieldInfo { get; set; }
+        public object FieldData { get; set; }
+
+        public ClFieldData(ClField fieldInfo, object fieldData)
+        {
+            FieldInfo = fieldInfo;
+            FieldData = fieldData;
+        }
+
+        public ClFieldData(ClField fieldInfo) : this(fieldInfo, Activator.CreateInstance(fieldInfo.FieldType))
+        {
+
+        }
+
+        public ClFieldData() : this(new ClField())
+        {
+
+        }
+
+
+    }
+}
