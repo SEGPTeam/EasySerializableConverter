2e01b14d7b5505bc028d3bcee36e76c3954907a9
skymak12 <40482803+skymak12@users.noreply.github.com>
2018-11-19 22:10:26 +0800

    change: added tabulation for different languages

--- EasySerializableConverterUI/FieldUIs/UCInstanceEditor.Designer.cs
+++ EasySerializableConverterUI/FieldUIs/UCInstanceEditor.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace EasySerializableConverterUI
+﻿namespace EasySerializableConverterUI.FieldUIs
 {
     partial class UCInstanceEditor
     {
@@ -29,29 +29,55 @@
         private void InitializeComponent()
         {
             this.FLPMain = new System.Windows.Forms.FlowLayoutPanel();
+            this.TBName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FLPMain
             // 
-            this.FLPMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.FLPMain.Location = new System.Drawing.Point(0, 0);
+            this.FLPMain.AutoScroll = true;
+            this.FLPMain.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.FLPMain.Location = new System.Drawing.Point(6, 31);
             this.FLPMain.Name = "FLPMain";
-            this.FLPMain.Size = new System.Drawing.Size(761, 737);
+            this.FLPMain.Size = new System.Drawing.Size(784, 703);
             this.FLPMain.TabIndex = 0;
+            this.FLPMain.WrapContents = false;
+            // 
+            // TBName
+            // 
+            this.TBName.Location = new System.Drawing.Point(126, 3);
+            this.TBName.Name = "TBName";
+            this.TBName.Size = new System.Drawing.Size(294, 22);
+            this.TBName.TabIndex = 1;
+            this.TBName.Text = "Unknown";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Language Name:";
             // 
             // UCInstanceEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBName);
             this.Controls.Add(this.FLPMain);
             this.Name = "UCInstanceEditor";
-            this.Size = new System.Drawing.Size(761, 737);
+            this.Size = new System.Drawing.Size(790, 737);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel FLPMain;
+        private System.Windows.Forms.TextBox TBName;
+        private System.Windows.Forms.Label label1;
     }
 }
