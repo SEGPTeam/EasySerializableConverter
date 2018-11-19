a8c75a9c4a9a77d90a5c974619390fde058f21a6
DESKTOP-C0CDTDA\chiuco <chiuco_dennis@hotmail.com>
2018-11-20 02:17:01 +0800

    Update UCInstanceEditor.Designer.cs

--- EasySerializableConverterUI/FieldUIs/UCInstanceEditor.Designer.cs
+++ EasySerializableConverterUI/FieldUIs/UCInstanceEditor.Designer.cs
@@ -35,40 +35,47 @@
             // 
             // FLPMain
             // 
+            this.FLPMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.FLPMain.AutoScroll = true;
             this.FLPMain.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.FLPMain.Location = new System.Drawing.Point(6, 31);
+            this.FLPMain.Location = new System.Drawing.Point(4, 23);
+            this.FLPMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.FLPMain.Name = "FLPMain";
-            this.FLPMain.Size = new System.Drawing.Size(784, 703);
+            this.FLPMain.Size = new System.Drawing.Size(588, 527);
             this.FLPMain.TabIndex = 0;
             this.FLPMain.WrapContents = false;
             // 
             // TBName
             // 
-            this.TBName.Location = new System.Drawing.Point(126, 3);
+            this.TBName.Location = new System.Drawing.Point(94, 2);
+            this.TBName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TBName.Name = "TBName";
-            this.TBName.Size = new System.Drawing.Size(294, 22);
+            this.TBName.Size = new System.Drawing.Size(222, 22);
             this.TBName.TabIndex = 1;
             this.TBName.Text = "Unknown";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Location = new System.Drawing.Point(2, 4);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.Size = new System.Drawing.Size(84, 12);
             this.label1.TabIndex = 2;
             this.label1.Text = "Language Name:";
             // 
             // UCInstanceEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TBName);
             this.Controls.Add(this.FLPMain);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UCInstanceEditor";
-            this.Size = new System.Drawing.Size(790, 737);
+            this.Size = new System.Drawing.Size(592, 553);
             this.ResumeLayout(false);
             this.PerformLayout();
 
