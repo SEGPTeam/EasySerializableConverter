2debda04f00bc35ca731afea653954be8948d659
skymak12 <40482803+skymak12@users.noreply.github.com>
2018-11-19 22:52:44 +0800

    add: tab function improvements

--- EasySerializableConverterUI/FmMain.Designer.cs
+++ EasySerializableConverterUI/FmMain.Designer.cs
@@ -29,31 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmMain));
-            this.FLPMain = new System.Windows.Forms.FlowLayoutPanel();
             this.BtnTestA = new System.Windows.Forms.Button();
             this.BtnTestB = new System.Windows.Forms.Button();
             this.BtnSave = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.TBInstanceName = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // FLPMain
-            // 
-            this.FLPMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.FLPMain.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.FLPMain.Location = new System.Drawing.Point(16, 50);
-            this.FLPMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.FLPMain.Name = "FLPMain";
-            this.FLPMain.Size = new System.Drawing.Size(1124, 556);
-            this.FLPMain.TabIndex = 0;
-            this.FLPMain.WrapContents = false;
             // 
             // BtnTestA
             // 
             this.BtnTestA.Location = new System.Drawing.Point(16, 15);
-            this.BtnTestA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnTestA.Margin = new System.Windows.Forms.Padding(4);
             this.BtnTestA.Name = "BtnTestA";
             this.BtnTestA.Size = new System.Drawing.Size(235, 28);
             this.BtnTestA.TabIndex = 1;
@@ -64,7 +50,7 @@
             // BtnTestB
             // 
             this.BtnTestB.Location = new System.Drawing.Point(259, 15);
-            this.BtnTestB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnTestB.Margin = new System.Windows.Forms.Padding(4);
             this.BtnTestB.Name = "BtnTestB";
             this.BtnTestB.Size = new System.Drawing.Size(297, 28);
             this.BtnTestB.TabIndex = 2;
@@ -76,7 +62,7 @@
             // 
             this.BtnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.BtnSave.Location = new System.Drawing.Point(1040, 614);
-            this.BtnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnSave.Margin = new System.Windows.Forms.Padding(4);
             this.BtnSave.Name = "BtnSave";
             this.BtnSave.Size = new System.Drawing.Size(100, 28);
             this.BtnSave.TabIndex = 3;
@@ -84,52 +70,52 @@
             this.BtnSave.UseVisualStyleBackColor = true;
             this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
             // 
-            // label2
+            // tabControl1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(605, 21);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Instance Name:";
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Location = new System.Drawing.Point(16, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1124, 557);
+            this.tabControl1.TabIndex = 7;
             // 
-            // TBInstanceName
+            // button1
             // 
-            this.TBInstanceName.Location = new System.Drawing.Point(723, 17);
-            this.TBInstanceName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.TBInstanceName.Name = "TBInstanceName";
-            this.TBInstanceName.Size = new System.Drawing.Size(327, 22);
-            this.TBInstanceName.TabIndex = 6;
+            this.button1.Location = new System.Drawing.Point(975, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add Language";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1156, 657);
-            this.Controls.Add(this.TBInstanceName);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.BtnSave);
             this.Controls.Add(this.BtnTestB);
             this.Controls.Add(this.BtnTestA);
-            this.Controls.Add(this.FLPMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FmMain";
             this.Text = "EasySerializableConverter";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.FlowLayoutPanel FLPMain;
         private System.Windows.Forms.Button BtnTestA;
         private System.Windows.Forms.Button BtnTestB;
         private System.Windows.Forms.Button BtnSave;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox TBInstanceName;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- EasySerializableConverterUI/FmMain.cs
+++ EasySerializableConverterUI/FmMain.cs
@@ -18,90 +18,42 @@ namespace EasySerializableConverterUI
             InitializeComponent();
             
         }
+        
 
-        private void DeployClass(ClClass input)
-        {
-            FLPMain.Controls.Clear();
-            InputClass = input;
-            foreach (var ele in input.Fields)
-            {
-                if (ele.GetType() == typeof(ClEnumerableField))
-                {
-                    var uc = new FieldUIs.UCEnumerableUI(ele);
-                    uc.Parent = FLPMain;
-                }
-                else
-                {
-
-                    var uc = new FieldUIs.UCPrimitiveUI(ele);
-                    uc.Parent = FLPMain;
-                }
-            }
-        }
-
-        //TODO these should be removed and replaced with a source file (.js .ts .cs) reader that outputs a ClClass instance.
         private void BtnTestA_Click(object sender, EventArgs e)
         {
-            DeployClass(TTestData.CreateTestClassPrimitive());
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "javascript(*.js) | *.js";
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                var ljson = new LoadJsFile();
+                InputClass = ljson.ReadJSFile(ofd.FileName);
+                button1.PerformClick();
+
+            }
         }
 
         private void BtnTestB_Click(object sender, EventArgs e)
         {
-            DeployClass(TTestData.CreateTestClassEnumerable());
         }
 
         private void BtnSave_Click(object sender, EventArgs e)
         {
-            var output = new ClClassData(TBInstanceName.Text, InputClass.ClassName, new List<ClFieldData>(), new List<ClEnumerableFieldData>());
 
-            bool error = false;
-            foreach (Control c in FLPMain.Controls)
+        }
+
+        public List<FieldUIs.UCInstanceEditor> LanguageList { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (InputClass != null)
             {
-                if (c is FieldUIs.UCPrimitiveUI)
-                {
-                    if ((c as FieldUIs.UCPrimitiveUI).Error == true)
-                    {
-                        error = true;
-                    }
-                }
-                else if (c is FieldUIs.UCEnumerableUI)
-                {
+                var ucie = new FieldUIs.UCInstanceEditor(InputClass);
+                ucie.Dock = DockStyle.Fill;
+                var tp = new TabPage("Language " + tabControl1.TabPages.Count.ToString());
+                tp.Controls.Add(ucie);
+                tabControl1.TabPages.Add(tp);
 
-                    if ((c as FieldUIs.UCEnumerableUI).Error == true)
-                    {
-                        error = true;
-                    }
-                }
-            }
-            
-
-            if (!error) // if there are no errors
-            {
-
-                foreach (Control c in FLPMain.Controls)
-                {
-                    if (c is FieldUIs.UCPrimitiveUI)
-                    {
-                        output.FieldDatas.Add((c as FieldUIs.UCPrimitiveUI).OutputData());
-                    }
-                    else if (c is FieldUIs.UCEnumerableUI)
-                    {
-                        output.EnumerableFieldDatas.Add((c as FieldUIs.UCEnumerableUI).OutputData());
-                    }
-                }
-                //Now output is ready to be serialized
-
-                var sfd = new SaveFileDialog();
-                sfd.Filter = "XML file as txt (*.txt)|*.txt|XML file as xml (*.xml)|*.xml";
-
-                if (sfd.ShowDialog() == DialogResult.OK)
-                {
-                    MdCore.Serialize<ClClassData>(output, sfd.FileName);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Error: there are syntax errors in the instance editor.");
             }
         }
     }
