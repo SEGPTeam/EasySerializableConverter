1ff694c2a6478794d68cd6055ee3d9879b61d488
DESKTOP-C0CDTDA\chiuco <chiuco_dennis@hotmail.com>
2018-11-19 20:48:42 +0800

    Updated by dennis added temp Create Json Class into project

---
+++ EasySerializableConverterLib/CreateJson.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace EasySerializableConverterLib
+{
+    class CreateJson
+    {
+
+        public void Json<T>(T obj,string path)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            var json = new JavaScriptSerializer().Serialize(obj);
+            Console.WriteLine(json);
+
+            using (StreamWriter sw = new StreamWriter(path))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, obj);
+
+            }
+        }
+
+        //For testing data use
+        public void function1()
+        {
+            MyDate aaa = new MyDate();
+            Json<MyDate>(aaa, @"C:\json.txt");
+            
+        }
+
+    }
+
+    public class MyDate
+    {
+        public int year;
+        public int month;
+        public int day;
+    }
+
+    public class Lad
+    {
+        public string firstName;
+        public string lastName;
+        public MyDate dateOfBirth;
+    }
+
+}
